52367fe95d0cd013c7d53cf5164391a52d12a565
Alexander Leon <1995.Alexander.Leon@gmail.com>
2020-11-05 13:00:07 -0800

    fixed some issues

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar.js'
 import useStyles from './App.styles'
 import Stack from './components/Stack/Stack.js'
-import Queue from './components/Queue/Queue.js'
-import LinkedList from './components/LinkedList/LinkedList.js'
-import BinarySearchTree from './components/BinarySearchTree/BinarySearchTree.js'
+
 
 
 function App() {
@@ -14,11 +12,7 @@ function App() {
    <Router>
      <Navbar />
      <main className={classes.app}>
-       <Route exact path='/' component={Stack}/>
-       <Route exact path='/stack' component={Stack}/>
-       <Route exact path='/queue' component={Queue}/>
-       <Route exact path='/linkedlist' component={LinkedList} />
-       <Route exact path='/binarysearchtree' component={BinarySearchTree} />
+      <Stack />
      </main>
    </Router>
   );
